edf080ff70d4a8f72bd6c77a4e242a3409b7f02c
Hassan AbuGareeb <Hassan.AbuGareeb@gmail.com>
2023-10-03 19:12:41 +0300

    task done

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+let totalBatteries=0;
+
+const reducer = (batteries) =>{
+   totalBatteries=batteries.reduce((totBattaries, battery)=>{
+        return totBattaries+battery;
+    },totalBatteries);
+}
 // Code your solution here
+reducer(batteryBatches);
+console.log(totalBatteries);
